6cba24da7896ded40a31fff0638c5e37b50dd2ee
adaatidev <ada.atienza@iesnervion.es>
2026-01-13 14:16:58 +0100

    Tercer boletín de arrays completado

---
+++ src/boletin03arraysbidimensionales/Ejercicio03.java
@@ -0,0 +1,23 @@
+package boletin03arraysbidimensionales;
+
+import java.util.Scanner;
+
+public class Ejercicio03 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * -----------------------------------------------------------------------------
+		 * .
+		 * -----------------------------------------------------------------------------
+		 */
+
+		// Creación del scanner para leer el teclado del usuario
+		Scanner sc = new Scanner(System.in);
+
+		// Cierre del scanner
+		sc.close();
+
+	}
+
+}
