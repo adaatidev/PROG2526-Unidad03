eb2eea9c49f7d63bb58e3d681c822532a5dd9ac0
adaatidev <ada.atienza@iesnervion.es>
2026-01-15 14:10:56 +0100

    Tercer boletín de arrays completado

--- src/boletin03arraysbidimensionales/Ejercicio04.java
+++ src/boletin03arraysbidimensionales/Ejercicio04.java
@@ -8,15 +8,32 @@ public class Ejercicio04 {
 
 		/*
 		 * -----------------------------------------------------------------------------
-		 * .
+		 * Escribe un programa que cree una tabla de 10x10 que contenga los valores de
+		 * las tablas de multiplicar del 1 al 10 (cada tabla de multiplicar en una
+		 * fila). Muestra la tabla por pantalla
 		 * -----------------------------------------------------------------------------
 		 */
 
-		// Creación del scanner para leer el teclado del usuario
-		Scanner sc = new Scanner(System.in);
+		// Variable para la multiplicación
+		int num = 1;
 
-		// Cierre del scanner
-		sc.close();
+		// Creación del array
+		int[][] array = new int[10][10];
+
+		// Bucle for que recorre las filas del array
+		for (int i = 0; i < array.length; i++) {
+
+			// Bucle for que recorre las columnas del array
+			for (int j = 0; j < array.length; j++) {
+				// Uso de las columnas para imprimir la multiplicación
+				array[i][j] = j + 1;
+				// Se imprime la multiplicación
+				System.out.print(array[i][j] + "*" + num + "=" + array[i][j] * num + "\t" + "\t");
+			}
+			// Se incrementa el auxiliar de la multiplicación
+			num++;
+			System.out.println();
+		}
 
 	}
 
